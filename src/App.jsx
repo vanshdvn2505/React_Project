0a8dee6b73e47a2492fe85ceeb6d9e09b13f9138
Vansh Dhawan <152591281+vanshdvn2505@users.noreply.github.com>
2024-03-23 20:39:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/utils/Header'
+import Form from '../components/utils/Form'
+function App() {
+    return (
+      <>
+        <Header></Header>
+        <Form></Form>
+      </>
+    )
+}
+
+export default App
